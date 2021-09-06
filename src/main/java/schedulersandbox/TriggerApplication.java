1614948ce1b0bbd9e86b7a976d15c95ea9e4c650
gvtortajada <vtortajada@google.com>
2021-09-06 21:02:21 +0000

    add build steps

--- src/main/java/schedulersandbox/TriggerApplication.java
+++ src/main/java/schedulersandbox/TriggerApplication.java
@@ -1,13 +1,30 @@
 package schedulersandbox;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class TriggerApplication {
+public class TriggerApplication implements CommandLineRunner{
+
+    private static Logger log = LoggerFactory
+        .getLogger(TriggerApplication.class);
+
+    @Autowired
+    private ApplicationContext context;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TriggerApplication.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("test2");
+        // SpringApplication.exit(this.context);
+    }
 
 }
