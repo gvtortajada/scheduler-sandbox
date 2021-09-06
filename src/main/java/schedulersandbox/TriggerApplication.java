e744b1b54f31066278dce41896f245d60353354d
gvtortajada <vtortajada@google.com>
2021-09-06 22:36:36 +0000

    add endpoint

--- src/main/java/schedulersandbox/TriggerApplication.java
+++ src/main/java/schedulersandbox/TriggerApplication.java
@@ -1,29 +1,33 @@
 package schedulersandbox;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class TriggerApplication implements CommandLineRunner{
+@RestController
+public class TriggerApplication {
 
-    @Autowired
     private ApplicationContext context;
-
-    @Autowired
     private MyService myService;
 
+    @Autowired
+    public TriggerApplication(ApplicationContext context, MyService myService){
+        this.context = context;
+        this.myService = myService;
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(TriggerApplication.class, args);
 	}
 
-    @Override
-    public void run(String... args) throws Exception {
+    @GetMapping("/")
+    public void trigger() throws InterruptedException {
         this.myService.exec();
         SpringApplication.exit(context);
     }
-
 
 }
